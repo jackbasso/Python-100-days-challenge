2e2fdd72304503e7562541bae04d0f3541749f5d
jackbasso <jackbasso.r@gmail.com>
2022-09-16 22:11:03 -0500

    day 6

--- Day 6/guess_word_letters.py
+++ Day 6/guess_word_letters.py
@@ -4,7 +4,8 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = word_list[random.randint(0,2)]
+#chosen_word = word_list[random.randint(0,2)]
+chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 print(chosen_word)
@@ -14,8 +15,8 @@ for _ in chosen_word:
 
 print(display)
 
-end = 0
-while end < 1:
+end_of_game = False
+while not end_of_game:
 	guess = input("Guess a letter:  ").lower()
 
 	#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
@@ -34,10 +35,10 @@ while end < 1:
 	for position in range(len(chosen_word)):
 		if guess == chosen_word[position]:
 			display[position] = guess
-
-	for blank in display:
-		if blank == "_":
-			end += 1
-
+	
 	print(display)
-
+	
+	if '_' not in display:
+		end_of_game = True
+			
+print("YOU WON!")	
