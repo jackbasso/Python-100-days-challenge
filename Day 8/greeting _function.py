635ed28677ceaa2e0c24b707c5b469086a9d2a36
jackbasso <jackbasso.r@gmail.com>
2022-09-17 18:09:15 -0500

    greeting function

---
+++ Day 8/greeting _function.py
@@ -0,0 +1,10 @@
+# Review: 
+# Create a function called greet().
+def greet(): 
+    # Write 3 print statements inside the function.
+    print("Hello Jack!")
+    print("Hello Jack, again!")
+    print("Hello Jack, again and again!")
+
+# Call the greet() function and run your code.
+greet()
