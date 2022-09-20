63a5441b1bc2f0512761f23e743c02ab7b194e26
jackbasso <jackbasso.r@gmail.com>
2022-09-19 22:02:04 -0500

    Day 9 high bid

--- Day 9 - Dictionaries/blind_auction.py
+++ Day 9 - Dictionaries/blind_auction.py
@@ -1,23 +1,39 @@
 import os
 #clear the output in the console.
 
-bids_list = []
-
 more_bidders = True
+# Create a dictionary
+bid_per_user = {'Test': 0}
+
 while more_bidders:
   name = input("What is your name?  ")
   bid_amount = float(input("What's your bid?  "))
   new_user = input("Are there any other bidder? Type 'yes' or 'no'  ")
-  # Create a dictionary
-  bid_per_user = {}
-  bid_per_user["user_name"] = name
-  bid_per_user["bid"] = bid_amount
-
-  # Add dictionary to list
-  bids_list.append(bid_per_user)
+  # clear console 
+  if new_user == "yes":
+    os.system("cls")
+  # add inputs to dict where name is the key of each bid
+  bid_per_user[name] = bid_amount
+  
+  # highest bidder manually, there are some methods like max and min
+  def highest_bidder():
+    '''
+    high_bid = "Test"
+    for key in bid_per_user:
+      if bid_per_user[key] > bid_per_user[high_bid]:
+        high_bid = key
+    print(bid_per_user[high_bid])
+    '''
+    bid = 0
+    high_bidder = ""
+    for key, value in bid_per_user.items():
+      if value > bid:
+        bid = value
+        high_bidder = key
+    print(f"The highest bid was made by: {high_bidder} for ${bid}") 
   
   if new_user == "no":
     more_bidders = False
+    highest_bidder()
 
-print(bids_list)
 
